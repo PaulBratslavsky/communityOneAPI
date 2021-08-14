75c859da68150bfb1484448e1b785ce60f88ab34
John Connor <johnconnor@Johns-MacBook-Pro.local>
2021-08-14 01:09:56 -0500

    first commit

--- config/database.js
+++ config/database.js
@@ -7,9 +7,9 @@ module.exports = ({ env }) => ({
         client: 'postgres',
         host: env('DATABASE_HOST', '127.0.0.1'),
         port: env.int('DATABASE_PORT', 5432),
-        database: env('DATABASE_NAME', 'strapiAPI'),
-        username: env('DATABASE_USERNAME', 'postgres'),
-        password: env('DATABASE_PASSWORD', 'Zealot1980!'),
+        database: env('DATABASE_NAME'),
+        username: env('DATABASE_USERNAME'),
+        password: env('DATABASE_PASSWORD'),
         ssl: env.bool('DATABASE_SSL', false),
       },
       options: {}
--- config/server.js
+++ config/server.js
@@ -3,7 +3,7 @@ module.exports = ({ env }) => ({
   port: env.int('PORT', 1337),
   admin: {
     auth: {
-      secret: env('ADMIN_JWT_SECRET', '2223247e948c78e0b8587ef938aac463'),
+      secret: env('ADMIN_JWT_SECRET'),
     },
   },
 });
