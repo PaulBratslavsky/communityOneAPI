c378cd1c6190fa53c101b4096802275bda46928d
John Connor <johnconnor@Johns-MacBook-Pro.local>
2021-08-14 01:24:14 -0500

    update database.js

--- config/database.js
+++ config/database.js
@@ -5,8 +5,8 @@ module.exports = ({ env }) => ({
       connector: 'bookshelf',
       settings: {
         client: 'postgres',
-        host: env('DATABASE_HOST', '127.0.0.1'),
-        port: env.int('DATABASE_PORT', 5432),
+        host: env('DATABASE_HOST'),
+        port: env.int('DATABASE_PORT'),
         database: env('DATABASE_NAME'),
         username: env('DATABASE_USERNAME'),
         password: env('DATABASE_PASSWORD'),
