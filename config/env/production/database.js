c6e7292469311c10ed2fa1e613ed662db3acc0f9
John Connor <johnconnor@Johns-MacBook-Pro.local>
2021-08-14 02:28:51 -0500

    deploy

--- config/env/production/database.js
+++ config/env/production/database.js
@@ -1,13 +1,13 @@
-const parse = require('pg-connection-string').parse;
+const parse = require("pg-connection-string").parse;
 const config = parse(process.env.DATABASE_URL);
 
 module.exports = ({ env }) => ({
-  defaultConnection: 'default',
+  defaultConnection: "default",
   connections: {
     default: {
-      connector: 'bookshelf',
+      connector: "bookshelf",
       settings: {
-        client: 'postgres',
+        client: "postgres",
         host: config.host,
         port: config.port,
         database: config.database,
@@ -23,25 +23,3 @@ module.exports = ({ env }) => ({
     },
   },
 });
- 
-
-
-
-module.exports = ({ env }) => ({
-    defaultConnection: 'default',
-    connections: {
-      default: {
-        connector: 'bookshelf',
-        settings: {
-          client: 'postgres',
-          host: env('DATABASE_HOST'),
-          port: env.int('DATABASE_PORT'),
-          database: env('DATABASE_NAME'),
-          username: env('DATABASE_USERNAME'),
-          password: env('DATABASE_PASSWORD'),
-          ssl: env.bool('DATABASE_SSL', true),
-        },
-        options: {}
-      },
-    },
-  });
--- config/env/production/server.js
+++ config/env/production/server.js
@@ -1,4 +1,3 @@
 module.exports = ({ env }) => ({
-    url: env('MY_HEROKU_URL'),
-  });
-   
+  url: env("MY_HEROKU_URL"),
+});
