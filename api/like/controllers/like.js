fcd82def3793efc6b63fc883292cebc4a7c019ce
Paul Bratslavsky <Paul.bratslavsky@gmail.com>
2021-09-25 01:27:33 -0500

    like controller code

--- api/like/controllers/like.js
+++ api/like/controllers/like.js
@@ -18,14 +18,14 @@ module.exports = {
         const project = await strapi.services.project.findOne({ id: projectID })
         if (!project) ctx.throw(400, "Post does not exist!")
 
-        const alreadyLiked = await strapi.services.like.findOne({ user: user.id, project: projectID })
+        const ifLiked = await strapi.services.like.findOne({ user: user.id, project: projectID })
 
-        if (alreadyLiked) {
+        if (ifLiked) {
             const id = { id: projectID }
             const data = { likesCount: project.likesCount - 1 }
             await strapi.services.project.update(id,data)
 
-            const entity = await strapi.services.like.delete({ id: alreadyLiked.id });
+            const entity = await strapi.services.like.delete({ id: ifLiked.id });
             return sanitizeEntity(entity, { model: strapi.models.like });
         } else {
             const id = { id: projectID }
@@ -38,11 +38,3 @@ module.exports = {
         
     },
 };
-
-// ctx.throw(400, "Already liked post!")
-            // const id = { id: projectID }
-            // const data = { likesCount: project.likesCount - 1 }
-            // await strapi.services.project.update(id,data)
-
-            // const entity = await strapi.services.like.delete({ id });
-            // return sanitizeEntity(entity, { model: strapi.models.like });
