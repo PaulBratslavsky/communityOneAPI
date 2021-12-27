b87683efa7aee50a6b99bd8786300f73712bd93b
Paul Bratslavsky <Paul.bratslavsky@gmail.com>
2021-12-27 15:34:45 -0500

    update-email-provider (#9)
    Co-authored-by: paul bratslavsky <paulbratslavsky@pauls-MBP.home>

--- config/plugins.js
+++ config/plugins.js
@@ -15,17 +15,19 @@ module.exports = ({ env }) => ({
     email: {
       provider: 'nodemailer',
       providerOptions: {
-        service: 'gmail',
+        host: env('SMTP_HOST'),
+        port: env('SMTP_PORT'),
         auth: {
           user: env('SMTP_USERNAME'),
           pass: env('SMTP_PASSWORD'),
         }
       },
       settings: {
-        defaultFrom: env('SMTP_USERNAME'),
-        defaultReplyTo: env('SMTP_USERNAME'),
+        defaultFrom: env('SMTP_FROM'),
+        defaultReplyTo: env('SMTP_TO'),
       },
     },
 
     // ...
   });
+
