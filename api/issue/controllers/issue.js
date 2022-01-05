612cea41497c691a6c6dc698f80a5fb4cf4ebdb0
Paul Bratslavsky <Paul.bratslavsky@gmail.com>
2022-01-04 23:24:23 -0500

    assign user and status on issue create

--- api/issue/controllers/issue.js
+++ api/issue/controllers/issue.js
@@ -1,8 +1,26 @@
-'use strict';
+const { parseMultipartData, sanitizeEntity } = require('strapi-utils');
 
-/**
- * Read the documentation (https://strapi.io/documentation/developer-docs/latest/development/backend-customization.html#core-controllers)
- * to customize this controller
- */
+module.exports = {
+  /**
+   * Create a record.
+   *
+   * @return {Object}
+   */
 
-module.exports = {};
+  async create(ctx) {
+    const { id } = ctx.state.user;
+    console.log(id)
+    let entity;
+    if (ctx.is('multipart')) {
+      const { data, files } = parseMultipartData(ctx);
+      console.log(data, "data")
+      entity = await strapi.services.issue.create({ ...data, createdBy: id, status: "NEW" }, { files });
+    } else {
+      console.log(ctx.request.body, "body")
+      entity = await strapi.services.issue.create({ ...ctx.request.body, createdBy: id, status: "NEW" });
+    }
+    return sanitizeEntity(entity, { model: strapi.models.issue });
+
+  },
+};
+ 
