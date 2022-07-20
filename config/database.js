687a24006dc33134e43d5925c6784fb85eb9b8df
Paul Bratslavsky <Paul.bratslavsky@gmail.com>
2022-07-19 21:55:19 -0500

    changed database to SQL lite

--- config/database.js
+++ config/database.js
@@ -4,15 +4,12 @@ module.exports = ({ env }) => ({
     default: {
       connector: 'bookshelf',
       settings: {
-        client: 'postgres',
-        host: env('DATABASE_HOST'),
-        port: env.int('DATABASE_PORT'),
-        database: env('DATABASE_NAME'),
-        username: env('DATABASE_USERNAME'),
-        password: env('DATABASE_PASSWORD'),
-        ssl: env.bool('DATABASE_SSL', true),
+        client: 'sqlite',
+        filename: env('DATABASE_FILENAME', '.tmp/data.db'),
       },
-      options: {}
+      options: {
+        useNullAsDefault: true,
+      },
     },
   },
 });
